f46007a5ee4a377ccd45d5e8d66fe5e33b42e629
Iminza67 <153586399+Iminza67@users.noreply.github.com>
2024-03-10 14:01:11 +0300

    Initial commit

---
+++ age calculator.py
@@ -0,0 +1,7 @@
+current = float(input("Enter the current year: "))
+birth = float(input("Enter your birth year: "))
+name = input("Enter your name: ")
+
+age = current - birth
+
+print( f"{name} is {age} years old")
